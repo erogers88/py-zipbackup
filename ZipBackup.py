529f8197e6f5c8247f5211e4f5e8b5be470d6d46
Erik Rogers <erikrogers88@gmail.com>
2021-07-04 10:32:26 -0400

    Made paths in zip file relative to source

--- ZipBackup.py
+++ ZipBackup.py
@@ -1,7 +1,12 @@
 import zipfile
 import datetime
 import os
-import argparse
+from pathlib import Path
+
+"""
+create_backup method can be used to back up an entire directory.
+"""
+
 
 def get_all_file_paths(directory):
     # initializing empty file paths list 
@@ -11,22 +16,33 @@ def get_all_file_paths(directory):
     for root, directories, files in os.walk(directory): 
         for filename in files: 
             # join the two strings in order to form the full filepath. 
-            full_path = os.path.join(root, filename) 
-            file_paths.append(full_path) 
-  
+            full_path = Path(os.path.join(root, filename))
+            file_paths.append(full_path)      
+    
     # returning all file paths 
     return file_paths  
 
-def create_backup(local_save_directory, remote_save_directory, file_prefix):
+def create_backup(source_location, backup_location, file_prefix):
+    """
+    source_location:     absolute path to directory to back up
+    backup_location:     absolute path to backup directory
+    file_prefix:         short prefix to add to beginning of zip file
+    """
 
+    # create path object to calculate relative path
+    source_path = Path(source_location)
+    
     # get all files in the input directory
-    all_file_paths = get_all_file_paths(local_save_directory)
+    all_file_paths = get_all_file_paths(source_path)
 
     # get current datetime and format as string
     current_date_time = datetime.datetime.now()
     time_stamp = '{:%Y-%m-%d_%H-%M-%S}'.format(current_date_time)
 
-    # write timestamped zip file in remote directory with all files
-    with zipfile.ZipFile(remote_save_directory + file_prefix + time_stamp + ".zip", 'w') as zip:
+    # write timestamped zip file in remote directory with all files in relative path
+    with zipfile.ZipFile(backup_location + file_prefix + time_stamp + ".zip", 'w') as zipped_file:
         for file in all_file_paths:
-            zip.write(file)
+            zipped_file.write(
+                file.absolute(), 
+                arcname=str(file.relative_to(source_path))
+            )
---
+++ test.py
@@ -0,0 +1,7 @@
+import ZipBackup
+
+source = "C:\\temp\\source\\"
+target = "C:\\temp\\target\\"
+prefix = "test_prefix"
+
+ZipBackup.create_backup(source, target, prefix)
