2e5a0896171c4d0263044106526fb12ed2122447
Erik Rogers <erik@rogers-family.net>
2021-11-20 20:46:22 -0500

    added unit-test

--- src/ZipBackup.py
+++ src/ZipBackup.py
@@ -1,8 +1,8 @@
-import zipfile
 import datetime
 import os
-from pathlib import Path
 import re
+import zipfile
+from pathlib import Path
 
 """
 create_backup method can be used to back up an entire directory.
@@ -25,10 +25,10 @@ def get_all_file_paths(directory):
 
 def create_backup(source_location, backup_location, file_prefix, extension=""):
     """
-    source_location:     absolute path to directory to back up
-    backup_location:     absolute path to backup directory
-    file_prefix:         short prefix to add to beginning of zip file
-    extension:           (optional) keep only files of this type
+    source_location:  absolute path to directory to back up
+    backup_location:  absolute path to backup directory
+    file_prefix:        short prefix to add to beginning of zip file
+    extension:        (optional) keep only files of this type
     """
     
     # create path object to calculate relative path
@@ -45,7 +45,7 @@ def create_backup(source_location, backup_location, file_prefix, extension=""):
     current_date_time = datetime.datetime.now()
     time_stamp = '{:%Y-%m-%d_%H-%M-%S}'.format(current_date_time)
 
-    # write timestamped zip file in remote directory with all files in relative path
+    # write timestamped zip file in remote directory with all matching files in relative path
     with zipfile.ZipFile(backup_location + file_prefix + time_stamp + ".zip", 'w') as zipped_file:
         for file in matching_paths:
             zipped_file.write(
